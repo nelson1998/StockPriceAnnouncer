14544913d5435406b200df447c3312ee6773de3f
Nelson <nelson.huang1998@gmail.com>
2019-07-13 23:14:21 -0400

    Implemented how often stock prices are announced

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyttsx3
 import bs4
 import requests
+import time
 
 engine = pyttsx3.init()
 
@@ -8,15 +9,18 @@ engine.setProperty("rate", 160)
 engine.setProperty("volume", 1.0)
 
 ticker = str(input("Enter a stock ticker."))
-final_price = ""
+mins = int(input("How often (in minutes)?"))
 
-stock = requests.get("https://robinhood.com/stocks/" + ticker)
-soup = bs4.BeautifulSoup(stock.text, "xml")
-price = soup.find("div", class_="_2YApulnV3lazBStOvoKx6m").find_all("span")
-name = soup.find("header", class_="Jo5RGrWjFiX_iyW3gMLsy").h1.text
+while True:
+    final_price = ""
+    stock = requests.get("https://robinhood.com/stocks/" + ticker)
+    soup = bs4.BeautifulSoup(stock.text, "xml")
+    price = soup.find("div", class_="_2YApulnV3lazBStOvoKx6m").find_all("span")
+    name = soup.find("header", class_="Jo5RGrWjFiX_iyW3gMLsy").h1.text
 
-for spans in price:
-    final_price = final_price + spans.string
+    for spans in price:
+        final_price = final_price + spans.string
 
-engine.say(name + " is now at: " + final_price)
-engine.runAndWait()
+    engine.say(name + " is now at: " + final_price)
+    engine.runAndWait()
+    time.sleep(float(mins * 60))
